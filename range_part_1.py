4ca8bdcce3e28d7eb7f1cae7df63f930ed7373dd
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-08-26 10:35:39 +0530

    Add files via upload

---
+++ range_part_1.py
@@ -0,0 +1,85 @@
+# a=range(6)
+# print(a,type(a))
+
+# b=range(9)
+# for lokesh in b:
+#     print(lokesh)  # here the range values give vertically from 0 to 8
+
+# for i in range(4):
+#     print(i)
+
+# for j in range(55):
+#     print(j)
+
+# for k in range(0,30):
+#     print(k)  # for whole numbers
+
+# for l in range(0,20,2):
+#     print(l)   # for even numbers 
+
+# for m in range(1,25,2):
+#     print(m)   # for odd numbers
+
+# for r in range(-1,15,1):
+#     print(r)
+
+# for s in range(-1,10,2):
+#     print(s)
+
+# for t in range(0,10,3):
+#     print(t,end="")
+# for t in range(0,10,3):
+#     print(t,end="*&_")
+
+# a=1,2,3,4,5
+# print(*a)
+
+# a=range(8)
+# print(a)
+
+# a=10,15,20,25
+# for l in a:
+#     print(a,end="abcd")  # here we used the method is "tuple" and then we got output as range in terms of "tuple"
+
+# for a in range(89):
+#     print(a)
+
+# n=int(input("Enter the value of n:"))
+# for l in range(1,n+1):
+#     print(l,end=" ")
+
+# for v in range(1,22,2):
+#     print(v,end="^")
+
+# for c in range(2,20,2):
+#     print(c,end=" ")
+
+# for l in range(10,1,-1):
+#     print(l)
+# for k in range(12,0,-1):
+#     print(k)
+
+# for h in range(10,-1,-1):
+#     print(h)
+
+# for r in range(10,-2,-1):
+#     print(r)
+
+# for lok in range(22,50,5):
+#     print(lok)
+
+# m=[1,23.4,'lokesh']
+# for l in m[::-1]:
+#     print(l)
+
+# madhu=[1,4.3,"lokesh"]
+# for lokesh in madhu[::2]:
+#     print(lokesh)
+
+# lok=[54,2.36,"hyd"]
+# for ameer in lok[::0]:
+#     print(ameer)   #  ValueError: slice step cannot be zero
+
+# ram=[89,0.87,"vinay"]
+# for sita in ram[::4]:
+#     print(sita)
