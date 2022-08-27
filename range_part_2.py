6eab43c08d7b5725c26da6e111a9338c192999f7
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-08-27 15:32:48 +0530

    Add files via upload

---
+++ range_part_2.py
@@ -0,0 +1,48 @@
+# n=int(input("Enter Any Integer Number:"))
+# for i in range(n):
+#     print(i)
+#     print(i)
+#     print(i)
+#     print("lokesh is a failure")
+# print("Love is More Dangerous")
+
+# n=int(input("Enter Any Integer Number:"))
+# for l in range(1,n):
+#     print(l)
+
+# k=int(input("Enter any integer Number:"))
+# for a in range(21,k):
+#     print(a,end="-")
+
+# l=int(input("Enter Any Integer Number:"))
+# for m in range(1,l,2):
+#     print(m)
+
+# b=int(input("Enter Any Integer Number:"))
+# if(b%2==0):
+#     print("{b} Even Number")
+# else:
+#     print("{b} Odd Number")
+
+# h=int(input("Enter Any Number:"))
+# if(h%5==0):
+#     print("{h} is Odd number")
+# else:
+#     print("{h} is Even number")
+
+# for i in range(1,5):
+#     print(i**i)
+# for i in range(1,5):
+#     print(i*i)
+
+# a=7
+# print(a)
+# a+=2
+# a-=6
+# print(a)
+
+l=int(input("Enter Any Number:"))
+while(l<10):
+    print("lokesh",str(l))
+    # l+=1
+    # print(l)
